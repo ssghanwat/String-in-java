e752f5cae70edc8137c155ee3033dde8506abc49
sandeep ghanwat <101173367+ssghanwat@users.noreply.github.com>
2022-07-11 00:06:08 +0530

    Add files via upload

--- StringXX.java
+++ StringXX.java
@@ -2,20 +2,18 @@ import java.io.*;
 
 class StringXX
 {
-    public static void main(String a[])
-    {
-        String str1 = "Hello";
-        String str2 = "World";
-        
-        System.out.println(str1);              //Hello
-        System.out.println(str1.length());    //5
-        
-        String str3 = str1+str2;
-        System.out.println(str3);
-        
-        String str4 = new String("Hello");
-        String str5 = new String("World");
-        String str6 = new String("Hello");
-        
-    }
+   public static void main(String a[])
+   {
+      int i=0;
+      String str = "Marvellous";
+      
+      System.out.println(str);
+      System.out.println(str.length());
+      
+      for(i=0;i<str.length();i++)
+      {
+         System.out.println(str.charAt(i));
+      }
+   
+   }
 }
